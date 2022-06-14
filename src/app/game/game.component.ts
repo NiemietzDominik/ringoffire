1ef9925c1b6c49be593a34c16f275cecb18e3968
Dominik Niemietz <d.niemietz97@gmx.de>
2022-06-14 21:22:05 +0200

    adding game, player components & game.ts

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game ;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+    console.log(this.game)
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard)
+        this.pickCardAnimation = false;
+      }, 1500)
+    }
+
+  }
+}
